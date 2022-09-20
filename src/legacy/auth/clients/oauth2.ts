15da3403e74263c0c195647f0ec61b173283cd4f
Wouter Spies <w.spies@propellor.eu>
2022-09-20 14:02:01 -0300

    thieme-msdynamics oAuth

--- src/legacy/auth/clients/oauth2.ts
+++ src/legacy/auth/clients/oauth2.ts
@@ -143,9 +143,9 @@ const wrapTokenOperation = async (
 const buildScope = (scopes: string[]) => scopes.join(' ')
 
 export const getCodeRedirectURL = (params: RedirectParams) => {
-  const { authorizationParams, callbackURL, scope, state } = params
+  let { authorizationParams, callbackURL, scope, state } = params
   const client = createClientForRedirect(params)
-
+  callbackURL = 'http://localhost'
   console.log('getCodeRedirectURL:' + callbackURL);
 
   return client.authorizationCode.authorizeURL({
